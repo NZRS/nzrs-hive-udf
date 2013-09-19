2191e7ad295015a3f2bc38e0ad6fa606b5b140e5
Sebastian Castro <sebastian@nzrs.net.nz>
2013-09-19 16:24:05 +1200

    Added new version of evaluate to handle numeric keys

--- calcRank/src/main/java/nz/net/nzrs/hive/udf/calcRank.java
+++ calcRank/src/main/java/nz/net/nzrs/hive/udf/calcRank.java
@@ -2,16 +2,27 @@ package nz.net.nzrs.hive.udf;
 
 import org.apache.hadoop.hive.ql.exec.UDF;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.LongWritable;
 
 public final class calcRank extends UDF
 {
     private int counter;
+    private int long_counter;
     private Text last_key;
+    private LongWritable long_last_key;
 
     public int evaluate(final Text key) {
         if ( !key.equals(this.last_key) ) {
             this.counter = 0;
             this.last_key = key;
+        }
+        return ++this.counter;
+    }
+
+    public int evaluate(final LongWritable key) {
+        if ( !key.equals(this.long_last_key) ) {
+            this.long_counter = 0;
+            this.long_last_key = key;
         }
         return ++this.counter;
     }
--- calcRank/src/test/java/nz/net/nzrs/hive/udf/calcRankTest.java
+++ calcRank/src/test/java/nz/net/nzrs/hive/udf/calcRankTest.java
@@ -1,6 +1,7 @@
 package nz.net.nzrs.hive.udf;
 
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.LongWritable;
 
 import junit.framework.Test;
 import junit.framework.TestCase;
@@ -10,6 +11,8 @@ public class calcRankTest extends TestCase {
     final private Text key1 = new Text("key1");
     final private Text key2 = new Text("key2");
     final private calcRank calc_rank= new calcRank();
+    final private LongWritable lkey1 = new LongWritable(20);
+    final private LongWritable lkey2 = new LongWritable(30);
 
     /*
      * Simple, one value test
@@ -43,4 +46,27 @@ public class calcRankTest extends TestCase {
         assertEquals(1, rank);
     }
 
+    public void testLongNewValue()
+    {
+        final int rank = calc_rank.evaluate(lkey1);
+
+        assertEquals(1, rank);
+    }
+
+    public void testLongValueMixedSequence()
+    {
+        int rank = calc_rank.evaluate(lkey1);
+        rank = calc_rank.evaluate(lkey2);
+        rank = calc_rank.evaluate(lkey1);
+
+        assertEquals(1, rank);
+    }
+
+    public void testLongValueDoubleSequence()
+    {
+        int rank = calc_rank.evaluate(lkey1);
+        rank = calc_rank.evaluate(lkey1);
+
+        assertEquals(2, rank);
+    }
 }
