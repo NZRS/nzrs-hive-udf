b267e84a8a0653dcbaf474f694390c6cbe44bced
Sebastian Castro <sebastian@nzrs.net.nz>
2013-10-24 15:07:17 +1300

    Bare archetype for a String simmilary set of functions for Hive

---
+++ Simmilarity/src/main/java/nz/net/nzrs/hive/udf/App.java
@@ -0,0 +1,13 @@
+package nz.net.nzrs.hive.udf;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
