bd914b6e895e3735680042ec7f775cbbc30775ac
Sebastian Castro <sebastian@nzrs.net.nz>
2014-03-25 09:39:39 +1300

    Version 0.1.1, with the MongeElkan testing fixed.

--- Simmilarity/src/test/java/nz/net/nzrs/hive/udf/MongeElkanDistanceTest.java
+++ Simmilarity/src/test/java/nz/net/nzrs/hive/udf/MongeElkanDistanceTest.java
@@ -8,12 +8,25 @@ import junit.framework.TestSuite;
 
 public class MongeElkanDistanceTest extends TestCase {
     final private Text text1 = new Text("home");
-    final private Text text2 = new Text("casa");
+    final private Text text2 = new Text("homer");
+    final private Text text3 = new Text("arthur");
     final private double epsilon = 0.0001;
 
     public void test1() {
         final double res = new MongeElkanDistance().evaluate(text1, text2);
-        final double expected = 0.15;
+        final double expected = 1.00;
+        assertEquals(expected, res, epsilon);
+    }
+
+    public void test2() {
+        final double res = new MongeElkanDistance().evaluate(text2, text3);
+        final double expected = 0.32;
+        assertEquals(expected, res, epsilon);
+    }
+
+    public void test3() {
+        final double res = new MongeElkanDistance().evaluate(text3, text3);
+        final double expected = 1.0;
         assertEquals(expected, res, epsilon);
     }
 }
